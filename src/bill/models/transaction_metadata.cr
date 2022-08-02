b445ba025586bdb750cd7e2e0398810038f0c98d
akadusei <attakusiadusei@gmail.com>
2022-08-02 15:45:11 +0000

    Replace `*::PRIMARY_KEY_TYPE` calls with `*::PrimaryKeyType`
    Helps get rid of macro calls in type restrictions.
    
    Related to commit 4b003128ff9a3b4118dd5b5ad7ed3b2cc1db7979.

--- src/bill/models/transaction_metadata.cr
+++ src/bill/models/transaction_metadata.cr
@@ -2,8 +2,8 @@ module Bill::TransactionMetadata
   macro included
     include Lucille::JSON
 
-    getter credit_note_id : {{ CreditNote::PRIMARY_KEY_TYPE }}?
-    getter invoice_id : {{ Invoice::PRIMARY_KEY_TYPE }}?
-    getter receipt_id : {{ Receipt::PRIMARY_KEY_TYPE }}?
+    getter credit_note_id : CreditNote::PrimaryKeyType?
+    getter invoice_id : Invoice::PrimaryKeyType?
+    getter receipt_id : Receipt::PrimaryKeyType?
   end
 end
