a485bf171f9843391430f61de6c89e9b7f333eab
akadusei <attakusiadusei@gmail.com>
2022-12-14 14:11:25 +0000

    Remove redundant method
    This was moved to *Lucille* a while ago.

--- spec/support/app/config/email.cr
+++ spec/support/app/config/email.cr
@@ -1,9 +1,3 @@
-private class DevDeliverLaterStrategy < Carbon::DeliverLaterStrategy
-  def run(email, &block)
-    block.call
-  end
-end
-
 BaseEmail.configure do |settings|
   settings.adapter = Carbon::DevAdapter.new
   settings.deliver_later_strategy = DevDeliverLaterStrategy.new
