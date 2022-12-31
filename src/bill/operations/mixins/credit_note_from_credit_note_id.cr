098bfe3e7abd940ffcf8cd3f31b9fa9ea5db7115
akadusei <attakusiadusei@gmail.com>
2022-12-31 19:27:45 +0000

    Define `#credit_note` only if not already defined

--- src/bill/operations/mixins/credit_note_from_credit_note_id.cr
+++ src/bill/operations/mixins/credit_note_from_credit_note_id.cr
@@ -1,7 +1,11 @@
 module Bill::CreditNoteFromCreditNoteId
   macro included
-    getter credit_note : CreditNote? do
-      credit_note_id.value.try { |value| CreditNoteQuery.new.id(value).first? }
-    end
+    {% unless @type.methods.find(&.name.== :credit_note.id) %}
+      getter credit_note : CreditNote? do
+        credit_note_id.value.try do |value|
+          CreditNoteQuery.new.id(value).first?
+        end
+      end
+    {% end %}
   end
 end
