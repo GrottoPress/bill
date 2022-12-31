f5d5c2bab60d996c304765e23b567f06fc683b80
akadusei <attakusiadusei@gmail.com>
2022-12-31 19:29:04 +0000

    Define `#invoice` only if not already defined

--- src/bill/operations/mixins/invoice_from_invoice_id.cr
+++ src/bill/operations/mixins/invoice_from_invoice_id.cr
@@ -1,7 +1,9 @@
 module Bill::InvoiceFromInvoiceId
   macro included
-    getter invoice : Invoice? do
-      invoice_id.value.try { |value| InvoiceQuery.new.id(value).first? }
-    end
+    {% unless @type.methods.find(&.name.== :invoice.id) %}
+      getter invoice : Invoice? do
+        invoice_id.value.try { |value| InvoiceQuery.new.id(value).first? }
+      end
+    {% end %}
   end
 end
