a704d7e76adf4af106919b6cad138d156f70fd93
akadusei <attakusiadusei@gmail.com>
2023-01-05 11:49:22 +0000

    Add `InvoiceTotals#credit_notes_fm`

--- src/bill/models/invoice_totals.cr
+++ src/bill/models/invoice_totals.cr
@@ -5,6 +5,10 @@ module Bill::InvoiceTotals
     getter credit_notes : Int32 = 0
     getter line_items : Int32 = 0
 
+    def credit_notes_fm : FractionalMoney
+      FractionalMoney.new(credit_notes)
+    end
+
     def line_items_fm : FractionalMoney
       FractionalMoney.new(line_items)
     end
