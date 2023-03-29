1b1db577af36ddf93903a3ecb03595dd238bc276
akadusei <attakusiadusei@gmail.com>
2023-03-29 15:50:26 +0000

    Fix potential bug
    Attribute types could be `Bool`, in which case they allowed to be
    `false`. This is not the case now, but fixing just in case.

--- src/bill/models/mixins/status_helpers.cr
+++ src/bill/models/mixins/status_helpers.cr
@@ -2,7 +2,7 @@ module Bill::StatusHelpers
   def now_finalized?(status : Avram::Attribute)
     status.value.try do |value|
       return false unless value.finalized?
-      !status.original_value || !status.original_value.try &.finalized?
+      status.original_value.nil? || !status.original_value.try(&.finalized?)
     end
   end
 end
